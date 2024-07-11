6405639ada2865063e15d1ceedb76d662e0082b7
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-11 13:43:26 +0200

    test: Add tests for Entrada and Grupo_Entrada classes - Added unit tests for creating Entrada objects based on age. - Included tests for error handling with negative age inputs in Entrada. - Implemented tests for Grupo_Entrada class to verify entry addition and total calculation. - Added tests to validate counting and subtotaling entries by ticket type.

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,4 +1,5 @@
-from models.model import Entrada, TipoEntrada
+from models.model import Entrada, TipoEntrada, Grupo_Entrada
+import pytest
 
 def test_crear_entrada():
     entrada = Entrada(12)
@@ -16,6 +17,50 @@ def test_crear_entrada():
     entrada = Entrada(1)
     assert entrada.tipo == TipoEntrada.BEBE
     assert entrada.precio == 0
+
+def test_crear_entrada_edad_negativa_error():
+    with pytest.raises(ValueError):
+        Entrada(-2)
+
+def test_crear_grupo_entradas():
+    grupo = Grupo_Entrada()
+    assert grupo.total == 0
+    assert grupo.num_entradas == 0
+
+def test_anyadir_entradas_a_grupo():
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(35)
+    assert grupo.num_entradas == 1
+    assert grupo.total == 23
+
+    grupo.add_entrada(12)
+    assert grupo.num_entradas == 2
+    assert grupo.total == 37
+
+    grupo.add_entrada(70)
+    assert grupo.num_entradas == 3
+    assert grupo.total == 55
+
+    grupo.add_entrada(2)
+    assert grupo.num_entradas == 4
+    assert grupo.total == 55
+
+def test_cantidad_entradas_por_tipo():
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(10)
+
+    assert grupo.cantidad_entradas_por_tipo(TipoEntrada.NIÑO) == 1
+
+    grupo.add_entrada(36)
+    assert grupo.cantidad_entradas_por_tipo(TipoEntrada.ADULTO) == 1
+
+    grupo.add_entrada(13)
+    assert grupo.cantidad_entradas_por_tipo(TipoEntrada.ADULTO) == 2
+
+def test_subtotal_por_tipo():
+    grupo = Grupo_Entrada()
+    grupo.add_entrada(10)
+    assert grupo.subtotal_tipo(TipoEntrada.NIÑO) == 14
     grupo = Grupo_Entrada()
     grupo.add_entrada(10)
     assert grupo.subtotal_tipo(TipoEntrada.NIÑO) == 14
