6f2f3e8277e21f0cdbc606bb1a9fa2a3a2c902c2
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-11 13:48:56 +0200

    feat: Add ticket types and ticket handling logic - Added TipoEntrada enum for different ticket types with associated prices. - Implemented Entrada class to handle ticket creation based on visitor age. - Implemented Grupo_Entrada class to manage groups of tickets, counting entries and calculating totals.

---
+++ models/model.py
@@ -0,0 +1,59 @@
+from enum import Enum
+
+class TipoEntrada(Enum):
+    BEBE = 0
+    NIÑO = 14
+    ADULTO = 23
+    JUBILADO = 18
+
+class Entrada:
+    def __init__(self, edad: int):
+        self.__validate_edad(edad)
+        self.__edad = edad
+        if edad <= 2:
+            self.tipo = TipoEntrada.BEBE
+            self.precio = 0
+        elif edad < 13:
+            self.tipo = TipoEntrada.NIÑO
+            self.precio = 14
+        elif edad < 65:
+            self.tipo = TipoEntrada.ADULTO
+            self.precio = 23
+        else:
+            self.tipo = TipoEntrada.JUBILADO
+            self.precio = 18
+
+    def __validate_edad(self, edad):
+        if edad < 0:
+            raise ValueError("La edad no debe ser negativa")
+        
+class Grupo_Entrada:
+
+    def __init__(self):
+        self.total = 0
+        self.num_entradas = 0
+        self.tipos_entrada = {
+            TipoEntrada.BEBE: {'P': 0, 'Q': 0},
+            TipoEntrada.NIÑO: {'P': 14, 'Q': 0},
+            TipoEntrada.ADULTO: {'P': 23, 'Q': 0},
+            TipoEntrada.JUBILADO: {'P': 18, 'Q': 0}
+        }
+
+    def add_entrada(self, edad):
+        """
+        En funcion de la edad, crear una entrada y incrementar el contador de entradas
+        Con el precio de la entrada nueva incrementar el total
+        """
+        nueva_entrada = Entrada(edad)
+        self.num_entradas += 1
+        self.total += nueva_entrada.precio
+
+        self.tipos_entrada[nueva_entrada.tipo]["Q"] += 1
+
+    def cantidad_entradas_por_tipo(self, tipo: TipoEntrada) -> int:
+        return self.tipos_entrada[tipo]["Q"]
+
+    def subtotal_tipo(self, tipo: TipoEntrada) -> int:
+        return self.tipos_entrada[tipo]["Q"] * self.tipos_entrada[tipo]["P"]
+    
+    
