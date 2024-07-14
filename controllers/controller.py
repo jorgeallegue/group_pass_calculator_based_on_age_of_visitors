bd455bba0ae03ac4d1c29cd75aed9df50b8ed99d
Jorge Allegue <jorgeallegue@gmail.com>
2024-07-14 19:45:46 +0200

    fix: Added error handling for invalid age inputs in controller.py and updated user prompts

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,7 +10,7 @@ def run_app(): #  Manages the main loop of the application
 
         vista_grupo = VistaGrupo(grupo_entradas, x, 1)
         entrada_edad = VistaEntrada("AGE: ", x, 10)
-        entrada_seguir = VistaEntrada("Do you want to start again? (Y/N): ", x, 12)
+        entrada_seguir = VistaEntrada("Do you want to start again?  (Y/N): ", x, 12)
 
         # Runs a loop to: 1. Display the group view. 2. Get user input for age and whether to restart. 3. Update the group of entrance tickets based on user input. 4. Handle restarting or exiting the application based on user input.
         while True:
@@ -26,9 +26,17 @@ def run_app(): #  Manages the main loop of the application
                 else:
                     break
             
-            edad = int(edad)
-            grupo_entradas.add_entrada(edad)
+            try:
+                edad = int(edad)
+                if edad < 0 or edad > 100:
+                    raise ValueError("Invalid age")
+                grupo_entradas.add_entrada(edad)
+            except ValueError as e:
+                locate(x, 12, f"Please enter an age between 0 and 100")
+                locate(x, 13, "Press enter to continue")
+                Input()
 
         # Final "controlado" del programa
         locate(1, DIMENSIONS.h - 2)
-        Input("Clic enter to exit")
+        locate(x, 13, "Clic enter to exit")
+        Input()
